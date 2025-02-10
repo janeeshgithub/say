ec7fc6f0ab517e2ce57ae8e3202bb47b3d536056
JANEESH P <janeeshthelegend@gmail.com>
2025-02-10 19:16:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const figlet = require("figlet");
+const chalk = require("chalk");
 const quotes = require("./quotes");
 
 const app = express();
